2be60d444c665812abb906b0602a5a4b7851eecc
Stefan <stefan.schlombs@gmail.com>
2021-09-21 23:09:08 +0200

    Prepare Insert

---
+++ Library/Container/Extension/Template/Parse/Insert/Insert/Insert.class.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+class ContainerExtensionTemplateParseInsertInsert extends ContainerExtensionTemplateParseInsert_abstract
+{
+    /**
+     * @var array
+     */
+    protected static array $insertion = [];
+
+    public static function load(): void
+    {
+        /** @var ContainerFactoryDatabaseQuery $query */
+        $query = Container::get('ContainerFactoryDatabaseQuery',
+                                __METHOD__ . '#select',
+                                true,
+                                ContainerFactoryDatabaseQuery::MODE_SELECT);
+
+        $query->setTable('template_positions');
+        $query->selectRaw('GROUP_CONCAT(crudContent SEPARATOR "") as crudContentConcat');
+        $query->select('crudPosition');
+        $query->groupBy('crudPosition');
+
+        $query->construct();
+        $smtp = $query->execute();
+
+        while ($smtpData = $smtp->fetch()) {
+            self::$positionInsertion[$smtpData['crudPosition']] = trim($smtpData['crudContentConcat']);
+        }
+
+        self::$positionInsertion['/_/base/url'] = Config::get('/server/http/base/url');
+    }
+
+    public static function insert(string $position, string $content): void
+    {
+        CoreDebugLog::addLog('/Template/Position/Insert',
+                             $position);
+
+        if (!isset(self::$positionInsertion[$position])) {
+            self::$positionInsertion[$position] = '';
+        }
+        self::$positionInsertion[$position] .= $content;
+    }
+
+    function parse(): string
+    {
+        $parameter = $this->getParameter();
+        $position  = (self::$positionInsertion[$parameter['position']] ?? ($parameter['default'] ?? ''));
+
+        CoreDebugLog::addLog('/Template/Position/Parse',
+                             $position);
+
+
+        return $position;
+    }
+
+    public static function add(string $key, string $value)
+    {
+        CoreDebugLog::addLog('/Template/Position/Add',
+                             $key . ' => ' . $value);
+
+
+        self::$positionInsertion[$key] = $value;
+    }
+}
